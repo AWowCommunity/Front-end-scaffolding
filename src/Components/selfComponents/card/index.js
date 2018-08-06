fe154bf7cb8da87aefe4b1ba10baf45b90b9d360
x_false <514286291@qq.com>
2018-08-06 18:08:19 +0800

    modify navpill components

--- src/Components/selfComponents/card/index.js
+++ src/Components/selfComponents/card/index.js
@@ -1,41 +1,78 @@
-import React, { Component } from "react"
-import styled from "styled-components"
-import Office from "./office2.jpg"
+import React, { Component } from "react";
+import styled from "styled-components";
+import Office from "./office2.jpg";
 
 const Container = styled.div`
-    height: 330px;
+  height: 330px;
+  border-radius: 6px;
+  color: #fff;
+  padding: 40px 20px;
+  background-image: url(${Office});
+  background-size: cover;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  &::after {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    display: block;
+    left: 0;
+    top: 0;
+    content: "";
+    background-color: rgba(0, 0, 0, 0.56);
     border-radius: 6px;
-    color: #fff;
-    background-image: url(${Office});
-    background-size: cover;
-    &::before {
-        position: absolute;
-        z-index: 1;
-        width: 100%;
-        height: 100%;
-        display: block;
-        left: 0;
-        top: 0;
-        content: "";
-        border-radius: 6px;
-        border:1px solid red;
-    }
-`
-
+  }
+`;
+const StyledH3 = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+  font-family: Roboto Slab, Times New Roman, serif;
+  margin: 20px 0;
+  z-index: 2;
+`;
+const StyledP = styled.div`
+  font-size: 14px;
+  font-weight: 300;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  margin-top: 10px;
+  color: hsla(0, 0%, 100%, 0.7) !important;
+  z-index: 2;
+`;
+const StyledH6 = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  color: hsla(0, 0%, 100%, 0.7) !important;
+  margin: 10px 0;
+  z-index: 2;
+`;
 class Card extends Component {
-    render() {
-        return (
-            <Container>
-                <h6>WORLDS</h6>
-                <h3>The Best Productivity Apps on Market</h3>
-                <p>
-                    Don't be scared of the truth because we need to restart the
-                    human foundation in truth And I love you like Kanye loves
-                    Kanye I love Rick Owens’ bed design but the back is...
-                </p>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <StyledH6>
+          {this.props.cardLable ? this.props.cardLable : "WORLDS"}
+        </StyledH6>
+        <StyledH3>
+          {this.props.cardTitle
+            ? this.props.cardTitle
+            : "The Best Productivity Apps on Market"}
+        </StyledH3>
+        <StyledP>
+          {this.props.cardContent
+            ? this.props.cardContent
+            : `Don't be scared of the truth because we need to restart the human
+            foundation in truth And I love you like Kanye loves Kanye I love Rick
+            Owens’ bed design but the back is...`}
+        </StyledP>
+      </Container>
+    );
+  }
 }
 
-export default Card
+export default Card;
--- src/Components/selfComponents/cardContainer/index.js
+++ src/Components/selfComponents/cardContainer/index.js
@@ -1,47 +1,56 @@
-import React, { Component } from "react"
-import styled from "styled-components"
-import Grid from "@material-ui/core/Grid"
-import { withStyles } from "@material-ui/core/styles"
-import PropTypes from "prop-types"
-import Card from '../card'
+import React, { Component } from "react";
+import styled from "styled-components";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import Card from "../card";
 
 const styles = theme => ({
-    root: {
-        flexGrow: 1
-    },
-    paper: {
-        padding: theme.spacing.unit * 2,
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-        position:'relative',
-    }
-})
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    // padding: theme.spacing.unit * 2,
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    position: "relative"
+  }
+});
 
-const Container = styled.div`
-`
+const Container = styled.div``;
 
 class CardContainer extends Component {
-    render() {
-        return (
-            <Container>
-                <Grid container spacing={24}>
-                    <Grid item xs={6}>
-                        <div className={this.props.classes.paper}>
-                            <Card />
-                        </div>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <div className={this.props.classes.paper}>xs=6</div>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <div className={this.props.classes.paper}>xs=12</div>
-                    </Grid>
-                </Grid>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Grid container spacing={24}>
+          <Grid item xs={6}>
+            <div className={this.props.classes.paper}>
+              <Card />
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className={this.props.classes.paper}>
+              <Card
+                cardLable="BUSINESS"
+                cardTitle="Working on Wallstreet is Not So Easy"
+              />
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div className={this.props.classes.paper}>
+              <Card
+                cardLable="MARKETING"
+                cardTitle="0 to 100.000 Customers in 6 months"
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  }
 }
 CardContainer.propTypes = {
-    classes: PropTypes.object.isRequired
-}
-export default withStyles(styles)(CardContainer)
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(CardContainer);
